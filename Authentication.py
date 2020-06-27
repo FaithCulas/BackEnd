61ebecd17949c9d4a9dba12f652a9e7fb6756b8d
Rozan Sonnadara <rozan.maria@gmail.com>
2020-06-27 23:13:45 +0530

    getUser

---
+++ Authentication.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def Authentication():
+    #getting preprocessed data
+	CSI = np.ndarray(10, dtype=np.complex128) 
+	amp = np.real(CSI)
+	ph = np.imag(CSI)
+
+	#presence detection
+	presence = getSVM(CSI)
+
+	#authentication
+	if presence == 1:
+	    user = authNetwork(CSI)
+	    #send user to front end
+	return user
+
+def getSVM(arr):
+    #using CSI values get features
+    #using the features send into SVM
+    #SVM predicts 1/0 for presence detection
+    prediction = 1
+    return prediction
+
+def authNetwork(arr):
+    #using CSI values get features
+    #using features send into trained network
+    #checking distance make prediction
+    #output user name or 'unknown'
+    prediction = 'Rozi'
+    return prediction
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,jsonify,request
 from flask_restful import Resource,Api
 from Activity import Activity
+from Authentication import Authentication
 
 app=Flask(__name__)
 api=Api(app)
@@ -8,8 +9,9 @@ api=Api(app)
 class Index(Resource):
     def get (self):
         activity=Activity()
+        authentication=Authentication()
         print(activity)
-        return ({"activity":str(activity),"user":"faith","location":"home"})
+        return ({"activity":str(activity),"user":str(authentication),"location":"home"})
 
 
 
