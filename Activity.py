d199d69f096d3a879cf73df4531a3a1332b4e2f6
FaithCulas <faithculas@gmail.com>
2020-06-30 17:37:44 +0530

    Faith's first commit

--- Activity.py
+++ Activity.py
@@ -1,2 +1,14 @@
+import pandas as pd
+import numpy as np
 def Activity():
-    return ("running")
+    """ activities=["running1","jumping"]
+    activity=activities[1]
+    return activity """
+    #WORDS = []
+    """ with open("A:\\GIT\\Backend\\env1\\test2.csv", "r") as file:
+        for line in file.readlines():
+            WORDS.append(line.rstrip()) """
+    WORDS=pd.read_csv("A:\\GIT\\Backend\\env1\\test2.csv")
+    values=WORDS.iloc[-1,:].values
+    activity=values[-1]
+    return activity
--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,redirect,url_for
 from flask_restful import Resource,Api
+from flask import Blueprint
 from Activity import Activity
-
 app=Flask(__name__)
 api=Api(app)
+ 
 
 class Index(Resource):
     def get (self):
         activity=Activity()
         print(activity)
         return ({"activity":str(activity),"user":"faith","location":"home"})
+    
+        """     WORDS = []
+            with open("A:\\GIT\\Backend\\env1\\test.csv2", "r") as file:
+                for line in file.readlines():
+                    WORDS.append(line.rstrip())
+            print (WORDS)
+            return WORDS """
+    
 
 
 
@@ -17,3 +26,4 @@ api.add_resource(Index,'/get')
 
 if __name__=='__main__':
     app.run(debug=True)
+    
