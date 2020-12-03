6950d2f492929610a1bc3f322d5710909db3fa5b
WiJE96 <you@example.com>
2020-12-03 22:22:10 +0530

    to suit lahiru's paths

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from flask_restful import Resource,Api
 from flask import Blueprint
 from flask_cors import CORS
 from Activity import Activity
+import numpy as np
+
 app=Flask(__name__)
+
+# activate VE: source ./env/bin/activate
+#run backend: /home/lahiru/Documents/Git/BackEnd/env/bin/python3 /home/lahiru/Documents/Git/BackEnd/app.py
 
 # Cross-Origin Resource Sharing <- allows react to access the API
 CORS(app)
@@ -21,15 +26,11 @@ content = data.read()
 users = content.splitlines() #have to change it to CSI data
 data.close()
 
+#specify user data file location
+userDataFile='/home/lahiru/Documents/Git/Data/userData.npy'
+
 class Index(Resource):
     def get(self):
-        activity=Activity()
-        from Authentication import Authentication
-        authentication=Authentication(users)
-        print(activity)
-        return ({"activity":str(activity),"user":str(authentication),"location":"home"})
-
-    def noob(self):
         activity=Activity()
         from Authentication import Authentication
         authentication=Authentication(users)
